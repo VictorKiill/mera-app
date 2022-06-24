be2a88ac1e081964ef4147140ad52d34642671e0
VictorKiill <victorkiill@gmail.com>
2022-06-23 21:17:24 -0300

    responsivity of body, layout and __next adjusted

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,7 +3,7 @@ import Header from "./header";
 
 export default function Layout({ children }) {
     return (
-        <div className="grid grid-rows-[5rem_1fr_5rem] h-full" >
+        <div className="grid grid-rows-[5rem_1fr_5rem] min-h-screen" >
             <Header />
             {children}
             <Footer />
